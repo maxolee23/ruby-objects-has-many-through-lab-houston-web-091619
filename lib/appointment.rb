51f65b49d2c5c3f39046427e63965a13ba6951fa
Max Lee <maxolee23@gmail.com>
2019-09-23 02:47:29 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,8 +1,14 @@
 class Appointment
 
+  attr_accessor :date, :patient, :doctor 
 
+  @@all = []
 
-
+  def initialize(date, patient, doctor)
+    @date = date 
+    @patient = patient 
+    @doctor = doctor 
+  end
 
 
 
